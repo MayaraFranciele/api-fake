d63d49a5c27d5bc1567b1bdd94c349a7aae896f1
MayaraFranciele <mayara.franciele39@gmail.com>
2023-07-06 16:38:59 -0300

    Adicionando estilos nas páginas

---
+++ assets/scripts/get.js
@@ -0,0 +1,34 @@
+function getProdutos() {
+            
+    fetch('http://localhost:3000/produtos', {
+        method: 'GET',
+        headers: {
+            'Content-type': 'application/json'
+        }
+    })
+        .then(resposta => resposta.json())
+        //    .then(resposta => JSON.stringify(resposta))
+        //   .then(resposta => JSON.parse(resposta))
+        .then(resposta => {
+
+            document.querySelector('#listaProdutos').innerHTML = "";
+
+
+            for (let i = 0; i < resposta.length; i++) {
+                const ul = document.createElement('ul');
+                ul.classList.add('produtos');
+                const img = document.createElement('img');
+                img.setAttribute('height', '80');
+
+                ul.appendChild(document.createElement('li')).innerHTML = resposta[i].id;
+                ul.appendChild(document.createElement('li')).innerHTML = resposta[i].descricao;
+                ul.appendChild(document.createElement('li')).innerHTML = resposta[i].preco;
+                ul.appendChild(document.createElement('li')).appendChild(img).setAttribute('src', resposta[i].imagem);
+            
+
+                document.querySelector('#listaProdutos').appendChild(ul);
+
+            }
+
+        });
+}
